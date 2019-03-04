febb943a62a4b1602cc4ef6265ac56fc64cd9e45
Yuchen Lu <luyuchen@mila01.server.mila.quebec>
2019-03-04 17:22:49 -0500

    ignore log

--- mila_run.sh
+++ mila_run.sh
@@ -4,5 +4,8 @@ export PYTHONUNBUFFERED=1
 source /network/home/luyuchen/miniconda2/etc/profile.d/conda.sh
 conda activate py36
 
-python main.py
+#python main.py --bptt 70 --save bptt70 --log-interval 50 > bptt70.log
+python main.py --bptt 1 --save bptt1 --log-interval 50 > bptt1.log
+
+
 
