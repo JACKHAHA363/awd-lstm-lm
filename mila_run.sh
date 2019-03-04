77d4d73f313b50f2b9d1bd88effe890d2cfba22c
Yuchen Lu <luyuchen@server.mila.quebec>
2019-03-04 17:36:46 -0500

    script

--- mila_run.sh
+++ mila_run.sh
@@ -4,8 +4,10 @@ export PYTHONUNBUFFERED=1
 source /network/home/luyuchen/miniconda2/etc/profile.d/conda.sh
 conda activate py36
 
-#python main.py --bptt 70 --save bptt70 --log-interval 50 > bptt70.log
-python main.py --bptt 1 --save bptt1 --log-interval 50 > bptt1.log
+rm *.log *.pt
+
+python main.py --bptt 70 --save bptt70.pt --log-interval 50 > bptt70.log
+python main.py --bptt 1 --save bptt1.pt --log-interval 50 --batch_size 512 > bptt1.log
 
 
 
